9cfc17fe23480c66108490823f74e660885a8585
Reginald Richardson <youngreg21@gmail.com>
2020-02-25 07:51:32 -0500

    Commit for CoffeShop MVC

---
+++ Controllers/UserRegistrationController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoffeShopMVC.Models;
+
+namespace CoffeShopMVC.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Get the form
+        [HttpGet]
+        public IActionResult RegistrationForm()
+        {
+            //returns RegistrationForm view
+            return View();
+        }
+
+        //Post the forms information to thee browser
+        [HttpPost]
+        public IActionResult WelcomePage(RegisterUser newUser)
+        {
+            //checks if data entered fits the model
+            if (ModelState.IsValid)
+            {
+                //returns the WelcomePage if the user input fits the form
+                return View("WelcomePage", newUser);
+            }
+            else
+            {
+                //returns the RegistrationForm again if user input doesn't fit the model
+                return View("RegistrationForm");
+            }
+        }
+
+
+    }
+}
---
+++ Models/RegisterUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace CoffeShopMVC.Models
+{
+    //Class for creating Users
+    public class RegisterUser
+    {
+        //property and property validation for UserName
+        [Required(ErrorMessage = "This field must have a value")]
+        [StringLength(30, MinimumLength = 4, ErrorMessage = "Your name must be between 4 and 30 characters")]
+        public string UserName { get; set; }
+
+        //property and property validation for Email
+        [Required(ErrorMessage = "This field must have a value")]
+        [RegularExpression(@"([A-Za-z]|[0-9]){5,30}(['@'])([A-Za-z]|[0-9]){5,10}(['.'])([A-Za-z]){1,3}", ErrorMessage = "Invalid Email entered")]
+        public string Email { get; set; }
+
+        //property and property validation for Password
+        [Required(ErrorMessage = "This field must have a value")]
+        [RegularExpression(@"([a-zA-Z]){1}[0-9a-zA-Z]{7,}", ErrorMessage = "Password must start with a letter and be at least 8 characters long")]
+        public string Password { get; set; }
+
+    }
+}
